b6d9cfa3cfdc6872c7cfbffa6c616d4a7dec0246
uf-code <ugur.firat.developer@gmail.com>
2022-03-13 23:04:39 +0300

    transform rotate and translate has been used

--- 02-delivery_driver_2d/Assets/Driver.cs
+++ 02-delivery_driver_2d/Assets/Driver.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        transform.Rotate(0, 0, 45);
+        // transform.Rotate(0, 0, 90);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, 0, 0.1f);
+
         // transform.Rotate(0, 0, -0.1f);
+
+        transform.Translate(0, -0.01f, 0);
+
     }
 }
