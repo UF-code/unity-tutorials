b9b062a1567728c10783efda78ef26ca94820646
uf-code <ugur.firat.developer@gmail.com>
2022-03-15 22:39:47 +0300

    adding delay for the loading scene

--- 03-snow_boarder_2d/Assets/Scripts/CrashDetector.cs
+++ 03-snow_boarder_2d/Assets/Scripts/CrashDetector.cs
@@ -5,11 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float sceneDelay = .5f;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", sceneDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- 03-snow_boarder_2d/Assets/Scripts/FinishLine.cs
+++ 03-snow_boarder_2d/Assets/Scripts/FinishLine.cs
@@ -6,11 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float sceneDelay = .5f;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", sceneDelay);
+
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
