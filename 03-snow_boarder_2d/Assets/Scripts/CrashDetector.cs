1758204c6b99f9da33c2c047031a14c567c0ce01
uf-code <ugur.firat.developer@gmail.com>
2022-03-15 23:50:52 +0300

    adding crash and finish effect on to the components

--- 03-snow_boarder_2d/Assets/Scripts/CrashDetector.cs
+++ 03-snow_boarder_2d/Assets/Scripts/CrashDetector.cs
@@ -5,11 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+
     [SerializeField] float sceneDelay = .5f;
+    [SerializeField] ParticleSystem crashEffect;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke("ReloadScene", sceneDelay);
         }
     }
--- 03-snow_boarder_2d/Assets/Scripts/FinishLine.cs
+++ 03-snow_boarder_2d/Assets/Scripts/FinishLine.cs
@@ -7,10 +7,12 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float sceneDelay = .5f;
+    [SerializeField] ParticleSystem finishEffect;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+            finishEffect.Play();
             Invoke("ReloadScene", sceneDelay);
 
         }
